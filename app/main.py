bcb1548716bc3163c7dcc48dc206aa7fba3a2046
nagpalpranay <54006833+nagpalpranay@users.noreply.github.com>
2024-06-11 18:56:29 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 import socket
 
 
+def extract_str(data):
+    decoded_data = data.decode().split('\r\n')
+    
+    # need error handling here
+    try:
+        str = decoded_data[0].split(' ')[1].split('/')[2]
+        return str
+    except:
+        return None
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -14,10 +24,16 @@ def main():
             data = conn.recv(4096)
             if not data:
                 break
+            str = extract_str(data)
             if "GET / " in data.decode('utf-8'):
                 conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-            else: 
+            elif str is None:
                 conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            else:
+                format = "text/plain"
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: {format}\r\nContent-Length: {len(str)}\r\n\r\n{str}"
+                conn.send(response.encode())
+                
        
     
     
