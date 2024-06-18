3f8dbef32214236b7b53a961d988f57a8f0f51b4
nagpalpranay <54006833+nagpalpranay@users.noreply.github.com>
2024-06-18 17:17:04 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,34 +3,27 @@ from threading import Thread
 import sys
 
 
-def extract(data:bytes)->str:
-    decoded_data = data.decode().split('\r\n')[0].split(' ')[1]
-
-    if decoded_data == "/":
-        return "/"
-    elif "/echo/" in decoded_data:
-        return decoded_data.split('/')[2]
-    elif decoded_data == "/user-agent":
-        return data.decode().split('\r\n')[2].split(' ')[1]
-    elif decoded_data.startswith("/files"):
-        return decoded_data
-    else:
-        return ""
-
-
-def response(str:str)->bytes:
-    response = ""
-    response += "HTTP/1.1"
-    Content_Type = "text/plain"
-    if str == "":
-        response += " 404 Not Found\r\n\r\n"
-    else:
-        response += " 200 OK\r\n"
-        if str == "/":
-            response += "\r\n"
-        elif str.startswith("/files"):
+def extract(data:bytes)->bytes:
+    if data=="":
+        return "HTTP/1.1 400 Not Found\r\n\r\n".encode()
+    
+    req = data.split('\r\n')
+    path = req[0].split(' ')[1]
+    type = req[0].split(' ')[0]
+    
+    
+    if type == "GET":
+        if path == "/":
+            return "HTTP/1.1 200 OK\r\n\r\n".encode()
+        elif path.startswith("/echo"):
+            str = path.split('/')[2]
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}".encode()
+        elif path == "/user-agent":
+            str = req[2].split(' ')[1]
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}".encode()
+        elif path.startswith("/files"):
             directory = sys.argv[2]
-            filename = str[7:]
+            filename = path[7:]
             Content_Type = "application/octet-stream"
             Content_Length = 0
             try:
@@ -38,19 +31,33 @@ def response(str:str)->bytes:
                     body = f.read()
                     Content_Length = len(body)
             except Exception as e:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
+                return "HTTP/1.1 404 Not Found\r\n\r\n".encode()
             else:
-                response += f"Content-Type: {Content_Type}\r\nContent-Length: {Content_Length}\r\n\r\n{body}"
+                return f"HTTP/1.1 200 OK\r\nContent-Type: {Content_Type}\r\nContent-Length: {Content_Length}\r\n\r\n{body}".encode()
         else:
-            response += f"Content-Type: {Content_Type}\r\nContent-Length: {len(str)}\r\n\r\n{str}"
-    return response.encode()
+            return "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+    elif type=="POST":
+        if path.startswith("/files"):
+            directory = sys.argv[2]
+            filename = path[7:]
+            Content_Type = req[3].split(' ')[1]
+            Content_Length = req[2].split(' ')[1]
+            body = req[5]
+            try:
+                with open(f"/{directory}/{filename}", "w") as f:
+                    f.write(body)
+            except Exception as e:
+                return "HTTP/1.1 500 Internal Server Error\r\n\r\n".encode()
+            else:
+                return "HTTP/1.1 201 Created\r\n\r\n".encode()
+
 
 
 def handle_client(conn:socket):
     try:
-        data = conn.recv(1024)
-        str = extract(data)
-        conn.send(response(str))
+        data = conn.recv(1024).decode()
+        response = extract(data)
+        conn.send(response)
     except Exception as e:
         print(f"handle_client error: {e}")
 
